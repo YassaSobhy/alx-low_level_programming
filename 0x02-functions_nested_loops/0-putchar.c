d870c7fd0422309daa102765e04fa611f0bd6516
YassaSobhy <yassasobhy321@gmail.com>
2023-07-18 13:55:07 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.c"
+/**
+ * main - Entry point
+ *
+ * Description: prints _putchar.c using putchar prototype
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar (str[ch]);
+	_putchar("\n");
+
+	return (0);
+}
