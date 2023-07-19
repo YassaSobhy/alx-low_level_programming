635a97626c0abcfc9325d93d01bef37ecaa550e2
YassaSobhy <yassasobhy321@gmail.com>
2023-07-19 16:36:15 -0700

    fsdgd

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,7 +6,6 @@
 int main(void)
 {
 int i;
-
 printf("Infinite loop incoming :(\n");
 i = 0;
 /*while (i < 10)*/
@@ -14,6 +13,5 @@ i = 0;
 /*putchar(i);*/
 /*}*/
 printf("Infinite loop avoided! \\o/\n");
-
 return (0);
 }
